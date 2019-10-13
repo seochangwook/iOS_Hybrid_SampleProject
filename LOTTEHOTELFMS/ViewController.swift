328260c8fc0af9d52bd1946e31e093ac5f6437f0
seochangwook <scw05313315@gmail.com>
2019-10-13 11:00:18 +0900

    init project

--- LOTTEHOTELFMS/ViewController.swift
+++ LOTTEHOTELFMS/ViewController.swift
@@ -7,14 +7,114 @@
 //
 
 import UIKit
+import WebKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate {
+    
+    @IBOutlet weak var webView: WKWebView!
+    
+    override func loadView() {
+        super.loadView()
+        
+        print("loadView Call")
+        
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        print("viewDidLoad Call")
+        
+        //WKWebview 셋팅
+        let url = URL(string: "https://www.naver.com/")
+        let request = URLRequest(url: url!)
+        
+        webView.load(request)
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        
+        print("viewDidAppear Call")
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func backbutton(_ sender: UIButton) {
+        print("back button click")
+        
+        webView.goBack()
+        webView.reload()
+    }
+    
+    @IBAction func forwardbutton(_ sender: UIButton) {
+        print("forward button click")
+        
+        webView.goForward()
+        webView.reload()
+    }
+    
+    @IBAction func refreshbutton(_ sender: UIButton) {
+        print("refresh button click")
+        
+        webView.reload()
+    }
+    
+    // -- WKUIDelegate 3가지 필수 함수 -- //
+    func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
+        let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        
+        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: {(action) in
+            completionHandler()
+        }))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    
+    func webView(_ webView: WKWebView, runJavaScriptConfirmPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (Bool) -> Void) {
+        let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
+        
+        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: { (action) in
+            completionHandler(true)
+        }))
+        
+        alertController.addAction(UIAlertAction(title: "취소", style: .default, handler: { (action) in
+            completionHandler(false)
+        }))
+        
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    func webView(_ webView: WKWebView, runJavaScriptTextInputPanelWithPrompt prompt: String, defaultText: String?, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping (String?) -> Void) {
+        let alertController = UIAlertController(title: "", message: prompt, preferredStyle: .alert)
+        
+        alertController.addTextField { (textField) in
+            textField.text = defaultText
+        }
+        
+        alertController.addAction(UIAlertAction(title: "확인", style: .default, handler: { (action) in
+            if let text = alertController.textFields?.first?.text {
+                completionHandler(text)
+            } else {
+                completionHandler(defaultText)
+            }
+        }))
 
+        alertController.addAction(UIAlertAction(title: "취소", style: .default, handler: { (action) in
+            completionHandler(nil)
+        }))
+
+        self.present(alertController, animated: true, completion: nil)
+    }
+    // ------------------------------ //
+    
+    // WKNavigationDelegate 중복적으로 리로드 방지 (iOS 9 이후지원)
+    public func webViewWebContentProcessDidTerminate(_ webView: WKWebView) {
+        webView.reload()
+    }
 }
-
