515bbbec24439fca806a41e891e5f82ca84d47d9
seochangwook <scw05313315@gmail.com>
2019-10-13 21:10:18 +0900

    js->native call fix

--- LOTTEHOTELFMS/ViewController.swift
+++ LOTTEHOTELFMS/ViewController.swift
@@ -9,23 +9,17 @@
 import UIKit
 import WebKit
 
-class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate{
-    
+class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate, WKScriptMessageHandler{
     @IBOutlet weak var webView: WKWebView!
+    @IBOutlet weak var outputtext: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
         print("viewDidLoad Call")
         
-        //WKWebview 셋팅
-        let url = URL(string: "http://192.168.0.12:8080/prototype/main.do")
-        let request = URLRequest(url: url!)
-        
-        webView.load(request)
-        
-        webView.uiDelegate = self
-        webView.navigationDelegate = self
+        initWebview_then_callFromJs()
+        loadUrl()
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -33,22 +27,7 @@ class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate{
         
         print("viewDidAppear Call")
         
-        ///Alert를 사용한 Network의 Check는 viewDidAppear에서 수행
-        if Reachability.isConnectedToNetwork(){
-            print("Network Connect")
-        } else{
-            print("Network Not Connect")
-            
-            let networkCheckAlert = UIAlertController(title: "Network ERROR", message: "앱을 종료합니다.", preferredStyle: UIAlertController.Style.alert)
-            
-            networkCheckAlert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction!) in
-                print("App exit")
-                
-                exit(0)
-            }))
-            
-            present(networkCheckAlert, animated: true, completion: nil)
-        }
+        checkNetworkConnect()
     }
     
     override func didReceiveMemoryWarning() {
@@ -80,7 +59,7 @@ class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate{
     @IBAction func homebutton(_ sender: UIButton) {
         print("home page reload")
         
-        webView.load(URLRequest(url: URL(string: "http://172.30.1.8:8080/prototype/main.do")!))
+        webView.load(URLRequest(url: URL(string: "http://172.30.1.7:8080/prototype/main.do")!))
     }
     
     @IBAction func jscallbutton(_ sender: UIButton) {
@@ -96,12 +75,12 @@ class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate{
     }
     
     @IBAction func jscallparambutton(_ sender: UIButton) {
-        //Parameter Setting//
+        ///Parameter Setting
         let name : String = "seochangwook"
         let age : Int = 28
         let address : String = "경기도 수원시"
         
-        /// Javascript Function Setting
+        /// Javascript Function Setting (Param)
         let testhybridparamfunc = "testhybridparam('\(name)', '\(age)', '\(address)')"
         
         /// Native -> JS Call (Param)
@@ -112,7 +91,63 @@ class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate{
         })
     }
     
-    // -- WKUIDelegate 3가지 필수 함수 -- //
+    func initWebview_then_callFromJs(){
+        ///Javascript Call Function Controller
+        let contentController = WKUserContentController()
+        let config = WKWebViewConfiguration()
+        
+        contentController.add(self, name : "hello")
+        contentController.add(self, name : "test")
+        
+        config.userContentController = contentController
+        
+        webView = WKWebView(frame: CGRect.init(x: 0, y: 86, width: 375, height: 404), configuration: config)
+    }
+    
+    func loadUrl(){
+        view.addSubview(webView)
+        
+        ///WKWebview 셋팅
+        let url = URL(string: "http://172.30.1.7:8080/prototype/main.do")
+        let request = URLRequest(url: url!)
+            
+        webView.load(request)
+        
+        webView.uiDelegate = self
+        webView.navigationDelegate = self
+    }
+    
+    func checkNetworkConnect(){
+        ///Alert를 사용한 Network의 Check는 viewDidAppear에서 수행
+        if Reachability.isConnectedToNetwork(){
+            print("Network Connect")
+        } else{
+            print("Network Not Connect")
+            
+            let networkCheckAlert = UIAlertController(title: "Network ERROR", message: "앱을 종료합니다.", preferredStyle: UIAlertController.Style.alert)
+            
+            networkCheckAlert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action: UIAlertAction!) in
+                print("App exit")
+                
+                exit(0)
+            }))
+            
+            self.present(networkCheckAlert, animated: true, completion: nil)
+        }
+    }
+    
+    /// WKScriptMessageHandler Callback (Javascript -> Native Call (Param))
+    func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
+        if(message.name == "hello"){
+            var data = message.body
+            outputtext.text = data as! String
+        } else if(message.name == "test"){
+            var data = message.body
+            outputtext.text = data as! String
+        }
+    }
+    
+    ///WKUIDelegate 3가지 필수 Callback 함수
     func webView(_ webView: WKWebView, runJavaScriptAlertPanelWithMessage message: String, initiatedByFrame frame: WKFrameInfo, completionHandler: @escaping () -> Void) {
         let alertController = UIAlertController(title: "", message: message, preferredStyle: .alert)
         
@@ -159,9 +194,9 @@ class ViewController: UIViewController, WKUIDelegate, WKNavigationDelegate{
 
         self.present(alertController, animated: true, completion: nil)
     }
-    // ------------------------------ //
+    ///----------
     
-    // WKNavigationDelegate 중복적으로 리로드 방지 (iOS 9 이후지원)
+    ///WKNavigationDelegate 중복적으로 리로드 방지 (iOS 9 이후지원)
     public func webViewWebContentProcessDidTerminate(_ webView: WKWebView) {
         webView.reload()
     }
